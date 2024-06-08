774b8af40b45e3e43bc3091e912827aae544e872
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2024-06-07 23:36:22 -0400

    Nice starting actix-web setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind on random port.");
+
+    let port = listener.local_addr().unwrap().port();
+    println!("Listening on address: http://127.0.0.1:{}", port);
+    run(listener)?.await
 }
