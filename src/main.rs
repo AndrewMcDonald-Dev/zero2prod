380852d68f089e368478b3a5f043c67c31223993
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2024-06-10 18:55:02 -0400

    Chapter 5: Push for Digital Ocean

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,14 @@ async fn main() -> Result<(), std::io::Error> {
     // Database setup
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     // Run App
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
